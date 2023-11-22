651aa86091deb61227deb7bd23af200306937f5c
Jonatas <joba.herc@gmail.com>
2023-11-22 16:38:00 -0300

    alteracoes

--- lib/view/coinlistView.dart
+++ lib/view/coinlistView.dart
@@ -1,9 +1,34 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:lista_cripto_facil/controller/coinController.dart';
 import 'package:lista_cripto_facil/controller/coinEntendide.dart';
 import 'package:provider/provider.dart';
 
-class CoinListView extends StatelessWidget {
+class CoinListView extends StatefulWidget {
+  @override
+  _CoinListViewState createState() => _CoinListViewState();
+}
+
+class _CoinListViewState extends State<CoinListView> {
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Inicia um timer que chama fetchCoinList a cada 5 segundos
+    _timer = Timer.periodic(Duration(seconds: 5), (Timer timer) async {
+      await Provider.of<CoinController>(context, listen: false).fetchCoinList();
+    });
+  }
+
+  @override
+  void dispose() {
+    // Cancela o timer quando o widget é descartado
+    _timer.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,17 +38,23 @@ class CoinListView extends StatelessWidget {
       body: Consumer<CoinController>(
         builder: (context, coinController, child) {
           return Center(
-            child: coinController.coinList != null
-                ? CoinList(cards: coinController.coinList!)
-                : CircularProgressIndicator(),
+            child: StreamBuilder<List<CoinEntidade>>(
+              stream: coinController.coinListStream,
+              initialData: coinController.coinList,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return Text('No data available');
+                } else {
+                  return CoinList(cards: snapshot.data!);
+                }
+              },
+            ),
           );
         },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Provider.of<CoinController>(context, listen: false).fetchCoinList();
-        },
-        child: Icon(Icons.refresh),
       ),
     );
   }
@@ -58,8 +89,10 @@ class CoinCard extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('ID: ${coinData.id}'),
-            Text('Type: ${coinData.type}'),
+            Text('ID: ${coinData.symbol}'),
+            Text('Name: ${coinData.name}'),
+            Text('Price USD: ${coinData.priceUsd}'),
+            Text('Volume 24h: ${coinData.volumeUsd24Hr}'),
             // Adicione mais campos conforme necessário
           ],
         ),
